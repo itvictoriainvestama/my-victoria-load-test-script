7f1e0398e668858275de142db9bed9d92a7b2d5d
ilhamhype <ilham@maxsol.id>
2024-09-25 10:31:54 +0700

    fix: profile

--- profile.sh
+++ profile.sh
@@ -5,8 +5,8 @@
 # export K6_WEB_DASHBOARD=true
 # k6 run ./src/customer/profile.js --summary-export ./result/profile/profile-1.json
 
-# export CUSTOMER_URL=https://vgh-api.otesuto.com/customer
-export CUSTOMER_URL=http://localhost:3700/customer/v0
+export CUSTOMER_URL=https://vgh-api.otesuto.com/customer
+# export CUSTOMER_URL=http://localhost:3700/customer/v0
 # export CUSTOMER_URL=https://api.myvictoria.co.id/customer
 export DIFFERENCE_CUSTOMER=true
 export K6_WEB_DASHBOARD=true
